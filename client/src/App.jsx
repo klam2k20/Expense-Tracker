d22278722fba629f94a0541ddac3b5b56fee184f
klam2k20 <klam2k17@gmail.com>
2023-01-20 23:55:02 -0500

    Frontend project structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import Chart from './components/Chart';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="App lg:max-w-screen-lg mx-auto drop-shadow-lg">
+      <h1 className="text-center text-white py-8 bg-slate-800 text-4xl xl:rounded">Expense Tracker</h1>
+      <div className="grid md:grid-cols-2 py-8">
+        <Chart />
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div className="flex justify-center bg-green-500">
+      <div className="max-w-sm bg-white">
+        Chart
+      </div>
+    </div>
+  );
+}
+
+export default Form;
